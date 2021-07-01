5edbb6a0e92358029f5ee8cdfeaaeb57b9d7881b
Piyush <impiyush016@gmail.com>
2021-07-01 16:25:55 +0530

    Added 08-lorem-ipsum-paragraph-generator

---
+++ 08-lorem-ipsum-generator/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import data from "./data";
+function App() {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+
+    setText(data.slice(0, amount));
+  };
+
+  const handleChange = (e) => {
+    let { value, min, max } = e.target;
+    value = Math.max(Number(min), Math.min(Number(max), Number(value)));
+
+    setCount(value);
+  };
+  return (
+    <section className="section-center">
+      <h3>tired of boring loren ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit} min={0} max={8}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          min="1"
+          max="9"
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+}
+
+export default App;
